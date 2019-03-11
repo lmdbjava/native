0e497b07788f6d6a4fa3e0be7e04dfab6d454f65
Greg Linklater <greglink49@gmail.com>
2019-03-11 08:44:08 +0000

    Add Android ARMv8 build target

---
+++ linux-android-arm64-v8a/src/main/java/org/lmdbjava/Holder.java
@@ -0,0 +1,9 @@
+package org.lmdbjava;
+
+/**
+ * Lightning Memory Database (LMDB) Native Wrapper.
+ * <p>
+ * This project uses a JAR solely for holding the native (C) shared LMDB library
+ * in a Java-accessible, Maven-mapped archive. There are no usable Java types.
+ */
+public class Holder {}
